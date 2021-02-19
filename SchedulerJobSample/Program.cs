4cf0efec93879387976ee8661887c909079d41cb
Ankit Vijay <ankit.vijay@zip.co>
2021-02-20 04:20:51 +1000

    Initial commit

---
+++ SchedulerJobSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchedulerJobSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
